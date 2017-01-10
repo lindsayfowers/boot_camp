82fbdf591f494665fe8df105a8ccbc54a47be4e5
lindsayfowers <lfowers@gmail.com>
2017-01-10 14:25:09 -0700

    Created about-me, contact, and root page. Created links for back, edit, and delete. Added main_image and thumb_image.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,12 +8,42 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     if @portfolio_item.save
       redirect_to @portfolio_item
     else
       render :new
     end
+  end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    if @portfolio_item.update(portfolio_params)
+      redirect_to @portfolio_item
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    redirect_to portfolios_path
+  end
+
+  private
+
+  def portfolio_params 
+    params.require(:portfolio).permit(:title, :subtitle, :body,:main_image, :thumb_image)
   end
 end
