def443187915533129483c37beaca48ab0f67fe4
lindsayfowers <lfowers@gmail.com>
2017-01-09 19:01:12 -0700

    Added portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,18 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    if @portfolio_item.save
+      redirect_to @portfolio_item
+    else
+      render :new
+    end
+  end
 end
