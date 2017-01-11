673c2894be77b365438612a4dc57d54374201273
lindsayfowers <lfowers@gmail.com>
2017-01-11 16:41:14 -0700

    Added status change button.

--- db/migrate/20170111203427_add_status_to_blog.rb
+++ db/migrate/20170111203427_add_status_to_blog.rb
@@ -1,5 +1,6 @@
-class AddStatusToBlog < ActiveRecord::Migration[5.0]
+ tclass AddStatusToBlog < ActiveRecord::Migration[5.0]
   def change
     add_column :blogs, :status, :integer, default: 0
   end
 end
+ 
