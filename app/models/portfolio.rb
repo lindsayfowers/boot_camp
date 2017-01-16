3fb1f8a2c3f89ec3dd8407725fd25aa209e20ab3
lindsayfowers <lfowers@gmail.com>
2017-01-16 14:08:29 -0700

    Set defaults, added placeholderconcerns

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -11,6 +11,12 @@ class Portfolio < ApplicationRecord
     where(subtitle: 'Angular')
   end
 
-  scope :rubu_on_rails_portfolio_items, ->{ where(subtitle: 'Ruby on Rails') }
+  scope :ruby_on_rails_portfolio_items, ->{ where(subtitle: 'Ruby on Rails') }
 
+  after_initialize: set_defaults
+
+  def set_defaults
+    self.main_image ||= Placeholder.image_generator(height: '600',width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350',width: '200')
+  end
 end
