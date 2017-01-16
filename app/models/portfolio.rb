ba652315d410c2d61a370e9393a35e74c7a90f23
lindsayfowers <lfowers@gmail.com>
2017-01-16 14:15:22 -0700

    corrected defaults/concerns

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,9 +1,6 @@
 class Portfolio < ApplicationRecord
   include Placeholder
 
-  has_many :technologies
-  accepts_nested_attributes_for :technologies
-                                reject_if: lambda { |attrs| attrs['name'].blank? }
 
   validates_presence_of :title, :body, :main_image, :thumb_image
 
@@ -13,7 +10,7 @@ class Portfolio < ApplicationRecord
 
   scope :ruby_on_rails_portfolio_items, ->{ where(subtitle: 'Ruby on Rails') }
 
-  after_initialize: set_defaults
+  after_initialize :set_defaults
 
   def set_defaults
     self.main_image ||= Placeholder.image_generator(height: '600',width: '400')
