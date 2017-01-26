f2e4f5853a0e908b8858f57464ff30d00400ec35
lindsayfowers <lfowers@gmail.com>
2017-01-26 15:43:58 -0700

    implement persistent position updates for drag and drop interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-
-  root to: 'pages#home'
-  
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+  resources :portfolios, except: [:show] do
+    put :sort, on: :collection
+  end
+  get 'angular-items', to: 'portfolios#angular'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'show_portfolio'
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
@@ -12,9 +14,6 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
-
-  resources :portfolios, except: [:show]
-  get 'portfolio/:id', to: 'portfolios#show', as: 'show_portfolio'
-  get 'angular-items', to: 'portfolios#angular'
  
+ root to: 'pages#home'
 end
