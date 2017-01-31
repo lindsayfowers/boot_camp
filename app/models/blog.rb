bf919e1dff052ae3cfc95744a4ee53c7854f8961
lindsayfowers <lfowers@gmail.com>
2017-01-31 12:01:58 -0700

    integrate cocoon for portfolio items nest form component

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,6 @@
 class Blog < ApplicationRecord
   validates_presence_of :title, :body
-  enum status: { draft: 0, published: 1}
+  enum status: { Draft: 0, published: 1}
   extend FriendlyId
   friendly_id :title, use: :slugged
 end
