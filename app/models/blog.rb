f0fb497e93f974f03eab4f236a697e4843d05f89
lindsayfowers <lfowers@gmail.com>
2017-02-02 11:03:25 -0700

    implement redis library and created comment generator.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,19 @@
 class Blog < ApplicationRecord
-  validates_presence_of :title, :body
   enum status: { Draft: 0, published: 1}
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  validates_presence_of :title, :body
+  
+  belongs_to :topic
+
+  has_many :comments, dependent: :destroy
+  
+  def self.special_blogs
+    all
+  end
+
+  def self.featured_blogs
+    limit(2)
+  end
 end
