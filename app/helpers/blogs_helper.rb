0d6966e6e537d17e1fbe391d626fb6e126d00b8a
lindsayfowers <lfowers@gmail.com>
2017-02-06 15:01:28 -0700

    implement markdown for blog posts

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -3,7 +3,23 @@ module BlogsHelper
     image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
   end
 
+  class CodeRayify < Redcarpet::Render::HTML 
+    def block_code(code, language)
+      CodeRay.scan(code, language).div
+    end
+  end
+
   def markdown(text)
-    Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(text).html_safe
+    coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
+
+    options = {
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      autolink: true,
+      lax_html_blocks: true,
+    }
+
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    markdown_to_html.render(text).html_safe
   end
 end
