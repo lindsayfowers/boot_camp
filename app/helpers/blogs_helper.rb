3f81e67531764dd3772fd5185ff253cb272645f4
lindsayfowers <lfowers@gmail.com>
2017-02-06 11:20:07 -0700

    implement custom scope for recent blogs and hide draft posts from non-admins

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,4 +2,8 @@ module BlogsHelper
   def gravatar_helper user
     image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
   end
+
+  def markdown(text)
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(text).html_safe
+  end
 end
