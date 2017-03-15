95fe2754681e59702178ca15b32a69a17ac1c33b
lindsayfowers <lfowers@gmail.com>
2017-03-15 12:00:32 -0600

    troubleshooting custom font

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module DevcampPortfolio
   class Application < Rails::Application
     config.eager_load_paths << "#{Rails.root}/lib"
     config.secret_key_base = ENV["SECRET_KEY_BASE"]
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
